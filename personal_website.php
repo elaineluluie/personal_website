13630ef4ab35b356ac5d755857eabe387f2f6244
elaineluluie <m25dt305@gmail.com>
2019-12-17 20:31:57 +0800

    add more photos

---
+++ personal_website.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+<?php echo '<p>Hello World</p>';
+$name=$Get["name"];
+$email=$Get["email"];
+$message=$Get["message"];
+
+print ($name)
+print ($email)
+print ($message)
+?>
+
+
+</body>
+</html>
